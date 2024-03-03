34294911c3a8076282bdb3e0178b64220e270d11
mohit patidar <mohitpatidar.1998.24@gmail.com>
2024-03-03 13:05:13 +0530

    dashboard model add

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+
+
 class BaseModel(models.Model):
     added_on = models.DateTimeField(default=timezone.now,db_index=True)
     updated_on = models.DateTimeField(auto_now=True,db_index=True)
@@ -38,3 +40,84 @@ class Create_Forex_Group(BaseModel):
     
 
 
+class Company_Profile(BaseModel):
+     name=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     logo=models.ImageField(null=True,upload_to='company/logo')
+     mobile_no=models.CharField(max_length=100,null=True,blank=True,db_index=True)
+     facebook_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     linkdin_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     whatsapp_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     telegram_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     instagram_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     twitter_link=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+
+     def __str__(self) -> str:
+          return self.name
+    
+
+class Email_Setting(BaseModel):
+     EMAIL_BACKEND=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     EMAIL_USE_TLS=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     EMAIL_HOST =models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     EMAIL_PORT =models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     EMAIL_HOST_USER =models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     EMAIL_HOST_PASSWORD=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+
+     def __str__(self) -> str:
+          return self.EMAIL_PORT
+     
+
+
+class Add_Platform_Link(BaseModel):
+       link_type = (('android link', 'Android Link'), ('ios link', 'Ios Link'), ('desktop link', 'Desktop Link'))
+       link= models.CharField( max_length=20,choices=link_type, default='live account',db_index=True)
+
+       def __str__(self) -> str:
+          return self.link
+       
+class Forex_Manager_Credential(BaseModel):
+     ip=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     login=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     password=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     def __str__(self) -> str:
+          return self.ip
+
+
+
+class Country(BaseModel):
+     shortname=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     name=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+     code=models.IntegerField(null=True,blank=True,db_index=True)
+     def __str__(self) -> str:
+          return self.shortname
+
+
+
+
+class Bank_Detail(BaseModel):
+    user_type = (('client', 'Client'), ('staff', 'Staff'), ('admin', 'Admin'))
+    user_type_name= models.CharField( max_length=20,choices=user_type, default='client',db_index=True)
+    benificary_name=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+    ifsc_code=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+    bank_name=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+    account_name=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+   
+    def __str__(self) -> str:
+          return self.benificary_name
+    
+
+
+
+class Wallet(BaseModel):
+    user_type = (('client', 'Client'), ('staff', 'Staff'), ('admin', 'Admin'))
+    user_type_name= models.CharField( max_length=20,choices=user_type, default='client',db_index=True)
+    wallet_type = (('usdt', 'USDT'), ('btc', 'BTC'), ('eth', 'ETH'))
+    wallet_type_name= models.CharField( max_length=20,choices=user_type, default='usdt',db_index=True)
+    wallet_address=models.CharField(max_length=200,null=True,blank=True,db_index=True)
+    
+
+    def __str__(self) -> str:
+          return self.wallet_type_name
+    
+
+    
--- forex_crm/urls.py
+++ forex_crm/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashborad',include('dashboard_app.urls') ),
+    path('',include('dashboard_app.urls') ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
