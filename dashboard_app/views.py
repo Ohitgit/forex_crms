dcd79af0c663f64a4b8c280d56be0c6faf12234b
mohit patidar <mohitpatidar.1998.24@gmail.com>
2024-03-03 14:58:52 +0530

    templpates page creted dashboard

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
    
 from django.http import HttpResponse
 from django.views import View
+from .froms import *
 # Create your views here.
 class Home(View):
-    def get(self, request):
-        return HttpResponse('result')
+    template_name="dashboard/index.html"
+    form=AddGroupForm()
+    def get(self, request,**kwargs):
+        print(self,'self')
+        context={'form':self.form}
+        return render(request,self.template_name,context)
+    
+    def post(self,request):
+        form = AddGroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
